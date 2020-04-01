249fc96413e31c65c13faa2a7b5213fe343392fd
Alan Cooney <alan@skyhookadventure.com>
2020-04-01 21:24:54 +0100

    Add basic firebase push notification support

--- android-app/app/src/main/java/com/coronotrace/coronavirustracker/MainActivity.java
+++ android-app/app/src/main/java/com/coronotrace/coronavirustracker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.coronotrace.coronavirustracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
@@ -10,21 +11,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
-
-import com.amazonaws.mobile.client.AWSMobileClient;
-import com.amazonaws.mobile.client.Callback;
-import com.amazonaws.mobile.client.UserStateDetails;
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
-import com.amplifyframework.api.graphql.MutationType;
 import com.amplifyframework.core.Amplify;
-import com.amplifyframework.datastore.generated.model.Contact;
-import com.amplifyframework.datastore.generated.model.Todo;
 import com.coronotrace.auth.AnonymousAuth;
-
-
-import java.util.UUID;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -141,6 +137,28 @@ public class MainActivity extends AppCompatActivity {
             mainButton.setText(R.string.made_mistake);
             mainButton.setBackgroundColor(backgroundColor);
         }
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+
+
+                });
+
     }
 
     /**
@@ -161,4 +179,5 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
     }
+
 }
