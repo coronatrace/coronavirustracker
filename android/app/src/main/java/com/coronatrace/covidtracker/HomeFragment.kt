89eb2c6b6d8bfb6b4581b7f56f35afbb5b0d0216
Alan Cooney <alan@skyhookadventure.com>
2020-04-05 16:17:23 +0100

    Make symptoms quiz stateful

--- android/app/src/main/java/com/coronatrace/covidtracker/HomeFragment.kt
+++ android/app/src/main/java/com/coronatrace/covidtracker/HomeFragment.kt
@@ -5,30 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class HomeFragment : Fragment() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        val view: View = inflater!!.inflate(R.layout.fragment_home, container, false)
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
+        val navigateCheckSymptomsButton = view.findViewById<Button>(R.id.navigateCheckSymptoms)
+        navigateCheckSymptomsButton.setOnClickListener { view -> view.findNavController().navigate(R.id.action_homeFragment_to_symptomsQuizFragment)}
 
-                }
-            }
+        return view
     }
 }
--- android/app/src/main/java/com/coronatrace/covidtracker/SymptomsQuizFragment.kt
+++ android/app/src/main/java/com/coronatrace/covidtracker/SymptomsQuizFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 
 /**
@@ -14,10 +18,10 @@ import android.view.ViewGroup
  */
 class SymptomsQuizFragment : Fragment() {
 
+    private lateinit var model: SymptomsQuizViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -25,15 +29,25 @@ class SymptomsQuizFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_symptoms_quiz, container, false)
+        val view: View = inflater!!.inflate(R.layout.fragment_symptoms_quiz, container, false)
+
+        // Get references
+        val questionTitle = view.findViewById<TextView>(R.id.questionTitle)
+        val questionBody = view.findViewById<TextView>(R.id.questionBody)
+        val buttonNo = view.findViewById<Button>(R.id.buttonNo)
+        val buttonYes = view.findViewById<Button>(R.id.buttonYes)
+
+        // Create the observer which updates the UI.
+        val questionObserver = Observer<QuizState> { question ->
+            questionTitle.setText(question.questionTitle)
+            questionBody.setText(question.questionBody)
+        }
+
+        model = ViewModelProvider(this).get(SymptomsQuizViewModel::class.java)
+        model.question.observe(this, questionObserver )
+
+        return view;
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SymptomsQuizFragment().apply {
-                arguments = Bundle().apply {
-                }
-            }
-    }
+
 }
