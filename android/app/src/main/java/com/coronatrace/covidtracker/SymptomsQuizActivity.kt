30f30a7336775db5497bc2efa9a5286a333c02f4
Alan Cooney <alan@skyhookadventure.com>
2020-04-05 09:08:38 +0100

    Add symptoms quiz activity

---
+++ android/app/src/main/java/com/coronatrace/covidtracker/SymptomsQuizActivity.kt
@@ -0,0 +1,12 @@
+package com.coronatrace.covidtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SymptomsQuizActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_symptoms_quiz)
+    }
+}
